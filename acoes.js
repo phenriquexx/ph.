fa9fe58d3412b130acd595072a420ec38e49fb66
phenriquexx <128866755+phenriquexx@users.noreply.github.com>
2023-05-17 09:36:04 -0300

    Add files via upload

---
+++ acoes.js
@@ -0,0 +1,19 @@
+function acao1(){
+    let resp = window.document.getElementById("saída")
+    resp.innerHTML += '<p> Clicou no primeiro botão </p>'
+}
+
+function acao2(){
+    let resp = window.document.getElementById("saída")
+    resp.innerHTML += '<p> Clicou no segundo botão </p>'
+}
+
+function acao3(){
+    let resp = window.document.getElementById("saída")
+    resp.innerHTML += '<p> Clicou no terceiro botão </p>'
+}
+
+function acao4(){
+    let resp = window.document.getElementById("saída")
+    resp.innerHTML += '<p> Clicou no quarto botão </p>'
+}
